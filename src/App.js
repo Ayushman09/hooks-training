6363200692fb9845a2ff612ffe17ecbf92ec36df
Ayushman09 <ayushmanchak@gmail.com>
2021-05-13 11:19:17 +0530

    React-hooks-training

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import ClassCounter from './components/ClassCounter';
+import ClassCounterOne from './components/ClassCounterOne';
+import ClassCounterTwo from './components/ClassCounterTwo';
+import HookCounter from './components/HookCounter';
+import HookCounterFour from './components/HookCounterFour';
+import HookCounterOne from './components/HookCounterOne';
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Class Counters</p>
+      <ClassCounter/>
+      <br></br>
+      <p>Hook Counter</p>
+      <HookCounter/>
+      <br></br>
+      <HookCounterTwo/>
+      <br></br>
+      Class Counter 
+      <ClassCounterTwo/>
+      <p>useState on objects</p>
+      <HookCounterThree/>
+      <p>UseState on Array</p>
+      <HookCounterFour/>
+      <p>useEffect example</p>
+      <ClassCounterOne/>
+      <br></br>
+      <HookCounterOne/>
     </div>
   );
 }
